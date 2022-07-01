0f91b3c21de302ed09a4e13e503d54da61657af0
Braden Rich <bradenwrich@gmail.com>
2022-07-01 16:27:59 -0400

    Create standard bot names

--- lib/bot.rb
+++ lib/bot.rb
@@ -2,7 +2,7 @@ require_relative 'player'
 
 class BotPlayer < Player
   attr_accessor :name, :hand, :books, :difficulty
-  NAMES = ['Roy', 'Jonathan', 'Logan', 'Rick Astley']
+  NAMES = ["Stacy's Mom", "Jesse's Girl", 'Rick Astley']
   def initialize(name: NAMES.sample, hand: [], books: [], difficulty: [])
     @name = name 
     @hand = hand
